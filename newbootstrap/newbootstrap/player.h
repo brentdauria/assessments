1dd329f93ebb2568d7c903c9b08ec226deceae3e
Brent <brentdauria@gmail.com>
2018-06-01 16:57:31 +1000

    this is the bootstrap

---
+++ newbootstrap/newbootstrap/player.h
@@ -0,0 +1,33 @@
+#pragma once
+#include<Input.h>
+#include <glm\vec2.hpp>
+#include <vector>
+#include "rock.h"
+
+namespace aie {
+	class Texture;
+	class Renderer2D;
+	class Input;
+	class Font;
+}
+
+class rock;
+
+class player
+{
+public:
+	player();
+	player(glm::vec2 pos);
+	~player();
+	void Update(float deltaTime, aie::Input* input);
+	void Draw(aie::Renderer2D* spritebatch);
+
+private:
+	glm::vec2	m_pos;
+	aie::Texture* m_texture;
+	float playerspeed = 320.0f;
+	rock* m_rock;
+	float distanceX = 0.0f;
+	float distanceY = 0.0f;
+};
+
