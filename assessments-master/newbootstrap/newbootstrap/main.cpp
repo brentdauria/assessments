6599380c92cf3abcc3b1b722c34523542a617bba
BRENT <Brentdauria@gmail.com>
2018-06-19 18:46:35 +1000

    Revert "no message"
    This reverts commit ce96a719b23a837e06e97c7ad614dd3d78b77299.

--- assessments-master/newbootstrap/newbootstrap/main.cpp
+++ assessments-master/newbootstrap/newbootstrap/main.cpp
@@ -3,19 +3,19 @@
 #include <Windows.h>
 #include <mmsystem.h>
 #include <stdlib.h>
-//#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "winmm.lib")
 using namespace std;
 
 int main() {
 	
-	//PlaySound(TEXT("../bin/Aleksander Vinter - Ism suite.wav"), NULL, SND_FILENAME | SND_ASYNC);
+	PlaySound(TEXT("../bin/Aleksander Vinter - Ism suite.wav"), NULL, SND_FILENAME | SND_ASYNC);
 
 	// allocation
 	auto app = new newbootstrapApp();
 
 	// initialise and loop
 	try {
-		app->run("speedy rocky", 1280, 720, false);
+		app->run("AIE", 1280, 720, false);
 	}
 	catch (exception e) {
 		cout << "game not working" << endl;
--- assessments-master/newbootstrap/newbootstrap/rock.cpp
+++ assessments-master/newbootstrap/newbootstrap/rock.cpp
@@ -28,10 +28,10 @@ void rock::Update(float deltaTime)
 	{
 		rockspeed = rockspeed * 1.1;
 		timer = 0.0f;
-		//if (rockspeed == 300)
-		//{
-		//	rockspeed = 290;
-		//}
+		if (rockspeed == 300)
+		{
+			rockspeed = 290;
+		}
 	}
 	if (m_pos.y >= 10 && !bouncey) {
 
