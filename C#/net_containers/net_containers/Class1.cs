65a4672bb69b92693c1f7d44630e67b3db4129ee
brent <brentdauria@gmail.com>
2018-08-09 16:46:54 +1000

    starting containers and window forms

---
+++ C#/net_containers/net_containers/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_containers
+{
+    class Class1
+    {
+        public void ArrayExample(int size)
+        {
+            int[] array = new int[size];
+            foreach(int index in array)
+            {
+                Console.WriteLine(array[index]);
+            }
+           
+        }
+
+    }
+}
