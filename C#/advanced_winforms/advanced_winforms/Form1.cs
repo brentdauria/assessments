f58e5ec418e4599f4124e61c69ba3066f9ef1a35
brent <brentdauria@gmail.com>
2018-08-22 17:20:35 +1000

    starting advanced controls

---
+++ C#/advanced_winforms/advanced_winforms/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace advanced_winforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+                  
+
+        private void FileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 newMDIChild = new Form1();
+            newMDIChild.MdiParent = this;
+            newMDIChild.Show();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ C#/advanced_winforms/advanced_winforms/MDIForm.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace advanced_winforms
+{
+    internal class MDIForm : Form
+    {
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MDIForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "MDIForm";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
