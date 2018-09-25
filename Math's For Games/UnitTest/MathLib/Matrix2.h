0d975bf9221fad0c58a88d94bb2a5d20d0d7b18e
brent <brentdauria@gmail.com>
2018-09-25 16:46:45 +1000

    no message

--- Math's For Games/UnitTest/MathLib/Matrix2.h
+++ Math's For Games/UnitTest/MathLib/Matrix2.h
@@ -1,7 +1,17 @@
 #pragma once
+#include "Vector3.h"
+
 class Matrix2
 {
 public:
+	union {
+		struct {
+			Vector3 xAxis;
+			Vector3 yAxis;
+			Vector3 zAxis;
+		};
+		Vector3 axis[3];
+	};
 	Matrix2();
 	~Matrix2();
 };
--- Math's For Games/UnitTest/MathLib/Matrix3.h
+++ Math's For Games/UnitTest/MathLib/Matrix3.h
@@ -1,10 +1,21 @@
 #pragma once
+#include "Vector3.h"
 
 class Matrix3
 {
 public:
+	union {
+		struct {
+			Vector3 xAxis;
+			Vector3 yAxis;
+			Vector3 zAxis;
+		};
+		Vector3 axis[3];
+		float data[3][3];
+	};
 	Matrix3();
 	~Matrix3();
+
 
 
 };
